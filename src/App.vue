be345e7bb74edc73a711a904aa2577a4e030371a
qiubee <45637654+qiubee@users.noreply.github.com>
2020-11-25 15:49:21 +0100

    add App.vue

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <Article />
+</template>
+
+<script>
+import Article from "./components/Article.vue"
+export default {
+	name: "App",
+	components: {
+		Article
+	}
+}
+</script>
+
+<style>
+</style>
