f442cbdabd6f4ebae6d2229dba51bb4460db5dff
qiubee <45637654+qiubee@users.noreply.github.com>
2020-11-25 15:49:31 +0100

    add Article component

---
+++ src/components/Article.vue
@@ -0,0 +1,23 @@
+<template>
+	<article>
+		<ArticleHeader />
+		<ChoroplethMap />
+	</article>
+</template>
+
+<script>
+import ChoroplethMap from "./ChoroplethMap.vue"
+import ArticleHeader from "./ArticleHeader.vue";
+
+export default {
+	name: "Article",
+	components: {
+		ChoroplethMap,
+		ArticleHeader
+	}
+}
+</script>
+
+<style>
+
+</style>
