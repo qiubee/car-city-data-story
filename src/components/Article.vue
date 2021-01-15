df57c8001522af63abfaac1310d806ea072cf7d8
qiubee <45637654+qiubee@users.noreply.github.com>
2021-01-15 19:17:47 +0100

    add markdown content & add title

--- src/components/Article.vue
+++ src/components/Article.vue
@@ -1,10 +1,22 @@
 <template>
 	<article>
 		<header>
-			<Markdown :content="intro" />
+			<h1>Nederland parkeerland?</h1>
 		</header>
-		<ProvinceMap />
-		<MunicipalityMap />
+		<section>
+			<Markdown :content="intro" />
+		</section>
+		<section>
+			<Markdown :content="provinces" />
+			<ProvinceMap />
+		</section>
+		<section>
+			<Markdown :content="municipalities" />
+			<MunicipalityMap />
+		</section>
+		<footer>
+			<Markdown :content="resources" />
+		</footer>
 	</article>
 </template>
 
@@ -13,6 +25,9 @@ import Markdown from "./Markdown.vue";
 import ProvinceMap from "./ProvinceMap.vue"
 import MunicipalityMap from "./MunicipalityMap.vue"
 import intro from "../assets/story/intro.md"
+import provinces from "../assets/story/parkeren-provincies.md"
+import municipalities from "../assets/story/parkeren-gemeenten.md"
+import resources from "../assets/story/bronnen.md"
 
 export default {
 	name: "Article",
@@ -23,11 +38,18 @@ export default {
 	},
 	data() {
 		return {
-			intro: String
+			intro: String,
+			provinces: String,
+			municipalities: String,
+			resources: String
 		}
 	},
 	created() {
 		this.intro = intro;
+		this.provinces = provinces;
+		this.municipalities = municipalities;
+		this.resources = resources;
+
 	}
 }
 </script>
