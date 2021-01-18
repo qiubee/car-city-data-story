a8db21b8d3259df9ca2e6647a7fd7a5a2b3abc44
qiubee <45637654+qiubee@users.noreply.github.com>
2021-01-18 23:45:52 +0100

    update website

---
+++ deploy.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env
+
+# abort on errors
+set -e
+# build dist
+npm run build
+
+# go to dist folder
+cd dist
+
+# push changes to gh-pages
+git add --all
+git commit -m "update website"
+git push origin gh-pages
+git checkout master
+
+# return to original folder
+cd- 
