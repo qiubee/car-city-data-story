8c0d59de88cb5dcaa7f0723192f3cce4850c7233
qiubee <45637654+qiubee@users.noreply.github.com>
2021-01-19 00:52:44 +0100

    update repository name

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-	publicPath: process.env.NODE_ENV === "production" ? "/frontend-applications/" : "/",
+	publicPath: process.env.NODE_ENV === "production" ? "/car-city-data-story/" : "/",
 	chainWebpack: function (config) {
 		config.module
 			.rule("md")
